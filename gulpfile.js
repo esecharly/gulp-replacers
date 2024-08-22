ce92eda50d0c70b4bab8644deaecac39bf6581e3
Carlos <62493210+esecharly@users.noreply.github.com>
2024-08-22 11:13:20 -0600

    actualizacion de reglas de reemplazo

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,50 @@
-const { src, dest, series } = require('gulp');
-const replace = require('gulp-replace');
+const { src, dest, series } = require("gulp");
+const replace = require("gulp-replace");
 
 const path = {
-    replaceHtml: 'inputs/**/*.html',
-    replaceTxt: 'inputs/**/*.txt'
+    replaceHtml: "inputs/**/*.html",
+    replaceTxt: "inputs/**/*.txt"
 }
 
 function replaceStringTxt() {
     return src(path.replaceTxt)
-        .pipe(replace('®', '(R)'))
-        .pipe(replace('©', '(C)'))
-        .pipe(dest('outputs/'));
+        .pipe(replace("®", "(R)"))
+        .pipe(replace("©", "(C)"))
+        .pipe(replace("PassTM", "Pass(TM)"))
+        .pipe(replace("Pass™", "Pass(TM)"))
+        /* .pipe(replace("%%email%%", "{(EMAIL)}"))
+        .pipe(replace("%%FNAME%%", "{(FNAME)}"))
+        .pipe(replace("%%INTL_LAST5%%", "{(LAST_5)}"))
+        .pipe(replace("%%ignore%%", ""))
+        .pipe(replace("http://ebm.email.americanexpress.com/c/tag/%%t%%/doc.html?t_sparams=%%t_sparams%%", "https://x.email.americanexpress.com/ats/msg.aspx?sg1={(URLSignature1)}")) */
+        
+        .pipe(dest("outputs/"));
 };
 
 function replaceStringHtml() {
     return src(path.replaceHtml)
-        .pipe(replace('á', '&aacute;'))
-        .pipe(replace('é', '&eacute;'))
-        .pipe(replace('í', '&iacute;'))
-        .pipe(replace('ó', '&oacute;'))
-        .pipe(replace('ú', '&uacute;'))
-        .pipe(replace('Á', '&Aacute;'))
-        .pipe(replace('É', '&Eacute;'))
-        .pipe(replace('Í', '&Iacute;'))
-        .pipe(replace('Ó', '&Oacute;'))
-        .pipe(replace('Ú', '&Uacute;'))
-        .pipe(replace('ñ', '&ntilde;'))
-        .pipe(replace('®', '<sup>&reg;</sup>'))
-        .pipe(replace('©', '&copy;'))
-        .pipe(replace('$', '&#36;'))
-        .pipe(dest('outputs/'));
+        .pipe(replace("á", "&aacute;"))
+        .pipe(replace("é", "&eacute;"))
+        .pipe(replace("í", "&iacute;"))
+        .pipe(replace("ó", "&oacute;"))
+        .pipe(replace("ú", "&uacute;"))
+        .pipe(replace("Á", "&Aacute;"))
+        .pipe(replace("É", "&Eacute;"))
+        .pipe(replace("Í", "&Iacute;"))
+        .pipe(replace("Ó", "&Oacute;"))
+        .pipe(replace("Ú", "&Uacute;"))
+        .pipe(replace("ñ", "&ntilde;"))
+        .pipe(replace("®", "<sup>&reg;</sup>"))
+        .pipe(replace("©", "&copy;"))
+        .pipe(replace("$", "&#36;"))
+        .pipe(replace("Pass™", "Pass<sup>TM</sup>"))
+        /* .pipe(replace("%%email%%", "{(email)}"))
+        .pipe(replace("%%FNAME%%", "{(FNAME)}"))
+        .pipe(replace("%%INTL_LAST5%%", "{(LAST_5)}"))
+        .pipe(replace("%%ignore%%", ""))
+        .pipe(replace("http://ebm.email.americanexpress.com/c/tag/%%t%%/doc.html?t_sparams=%%t_sparams%%", "https://x.email.americanexpress.com/ats/msg.aspx?sg1={(URLSignature1)}")) */
+        
+        .pipe(dest("outputs/"));
 };
 
 exports.replacerAll = series(replaceStringHtml, replaceStringTxt);
